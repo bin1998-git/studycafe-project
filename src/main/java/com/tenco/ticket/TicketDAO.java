44cc891ca9835dccf5b572bbaa8bd40c0fbca04e
이정민 <protain02@naver.com>
2026-04-14 14:35:28 +0900

    feat:ticket dto 설계

--- src/main/java/com/tenco/ticket/TicketDAO.java
+++ src/main/java/com/tenco/ticket/TicketDAO.java
@@ -1,4 +1,30 @@
 package com.tenco.ticket;
 
+import java.util.List;
+
 public class TicketDAO {
+    // 전체 이용권 상품 조회
+    public List<TicketDTO> findAll() {
+
+    }
+
+    // ID로 1건 조회
+    public TicketDTO findById(int ticketId) {
+
+    }
+
+    // 이용권 상품 등록
+    public boolean insert(TicketDTO ticketDTO) {
+
+    }
+
+    // 이용권 상품 수정
+    public boolean update(TicketDTO ticketDTO) {
+
+    }
+
+    // 이용권 상품 삭제
+    public boolean delete(int ticketId) {
+
+    }
 }
--- src/main/java/com/tenco/ticket/TicketDTO.java
+++ src/main/java/com/tenco/ticket/TicketDTO.java
@@ -1,4 +1,18 @@
 package com.tenco.ticket;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TicketDTO {
+    private int ticketId;
+    private String name;
+    private Enum type;
+    private int durationValue;
+    private int price;
+    private String description;
 }
