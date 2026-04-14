525543f76a0618b85464b689eca2172e2bbe8973
san <san1425865@gamil.com>
2026-04-14 17:46:26 +0900

    seatDAO 구현

--- src/main/java/com/tenco/seat/SeatDAO.java
+++ src/main/java/com/tenco/seat/SeatDAO.java
@@ -1,7 +1,6 @@
 package com.tenco.seat;
 
 
-
 import com.tenco.util.DBConnectionManager;
 
 import java.sql.Connection;
@@ -17,8 +16,8 @@ public class SeatDAO {
     public List<SeatDTO> findAll() throws SQLException {
         List<SeatDTO> seatList = new ArrayList<>();
         String sql = """
-        SELECT * FROM seat;
-        """;
+                SELECT * FROM seat;
+                """;
         try (Connection conn = DBConnectionManager.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
@@ -32,12 +31,12 @@ public class SeatDAO {
 
     // seatId 로 좌석 한 건 조회
     public SeatDTO findSeatById(int seatId) throws SQLException {
-        SeatDTO seatDTO  = new SeatDTO();
+        SeatDTO seatDTO = new SeatDTO();
         String sql = """
                 SELECT * FROM seat WHERE seat_id = ?
                 """;
         try (Connection conn = DBConnectionManager.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, seatId);
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
@@ -48,23 +47,70 @@ public class SeatDAO {
     }
 
     // 사용 가능한 자리 모두 조회
-    public List<SeatDTO> findAvailable() {
-        return null;
+    public List<SeatDTO> findAvailable() throws SQLException {
+        String sql = """
+                SELECT * FROM seat WHERE status = 'AVAILABLE'
+                """;
+        List<SeatDTO> seatList = new ArrayList<>();
+
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                seatList.add(mapToSeat(rs));
+            }
+
+        }
+        return seatList;
     }
 
     // 좌석 추가
-    public boolean insert(SeatDTO seatDTO) {
-        return false;
+    public boolean insert(SeatDTO seatDTO) throws SQLException {
+        String sql = """
+                INSERT INTO SEAT (seat_number, seat_type, status, zone) VALUES
+                    (?, ?, ?, ?)
+                """;
+        int rows;
+        try (Connection conn = DBConnectionManager.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, seatDTO.getSeatNumber());
+                pstmt.setString(2, seatDTO.getSeatType().toString());
+                pstmt.setString(3, seatDTO.getStatus().toString());
+                pstmt.setString(4, seatDTO.getZone());
+
+                rows = pstmt.executeUpdate();
+        }
+        return rows > 0;
     }
 
     // 좌석 상태 변경 (빈자리, 사용중인 자리등등)
-    public boolean updateStatus(int seatId, Status status) {
-        return false;
+    public boolean updateStatus(int seatId, Status status) throws SQLException {
+        String sql = """
+                UPDATE seat SET status = ? WHERE seat_id = ?
+                """;
+        int rows;
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, status.toString());
+            pstmt.setInt(2, seatId);
+            rows = pstmt.executeUpdate();
+        }
+        return rows > 0;
     }
 
     // 좌석 삭제
-    public boolean delete(int seatId) {
-        return false;
+    public boolean delete(int seatId) throws SQLException {
+        String sql = """
+                DELETE FROM seat WHERE seat_id = ?;
+                """;
+        int rows;
+
+        try (Connection conn = DBConnectionManager.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, seatId);
+            rows = pstmt.executeUpdate();
+        }
+        return rows > 0;
     }
 
 
