4ab3d0434548d87e3f9eeb689c2b1376e026f1d4
박효균 <gurbsl12@naver.com>
2026-04-15 15:42:06 +0900

    feat:payment DTO DAO 구현

--- src/main/java/com/tenco/payment/PaymentDTO.java
+++ src/main/java/com/tenco/payment/PaymentDTO.java
@@ -1,4 +1,21 @@
 package com.tenco.payment;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Builder
+@ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDTO {
+
+  private int payment_id; // 결제ID (PK)
+  private int member_id; // 회원ID (FK)
+  private int ticket_id; // 이용권 상품 ID (FK)
+  private int amount; // 결제금액
+  private String method; // card, cash, tansfer
+  private String status; // succeess, fail, refund
+  private LocalDateTime paidAt; // 결제 시각
+
 }
