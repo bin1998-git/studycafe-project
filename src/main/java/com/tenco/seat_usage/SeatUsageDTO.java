682197f5572230404f25e777d83b08c11d5afae3
san <san1425865@gamil.com>
2026-04-15 17:35:04 +0900

    입실 기록 생성 구현중

--- src/main/java/com/tenco/seat_usage/SeatUsageDTO.java
+++ src/main/java/com/tenco/seat_usage/SeatUsageDTO.java
@@ -1,12 +1,21 @@
 package com.tenco.seat_usage;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class SeatUsageDTO {
     private int usageId;
     private int memberId;
     private int seatId;
     private int memberTicketId;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
 }
