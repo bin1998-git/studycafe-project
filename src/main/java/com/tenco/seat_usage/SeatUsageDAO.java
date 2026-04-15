499595a8ecd8f1ed6d102a3fb765f24eecb5ac9c
bin1998-git <jbhan0217@naver.com>
2026-04-15 17:14:09 +0900

    feat:SeatUsageDTO,DAO /bulider구현

--- src/main/java/com/tenco/seat_usage/SeatUsageDAO.java
+++ src/main/java/com/tenco/seat_usage/SeatUsageDAO.java
@@ -1,5 +1,6 @@
 package com.tenco.seat_usage;
 
+import com.tenco.member.MemberDTO;
 import com.tenco.util.DBConnectionManager;
 
 import java.sql.Connection;
@@ -25,24 +26,60 @@ public class SeatUsageDAO {
         return false;
     }
 
+
     // 현재 이용중인 내역 조회
     public SeatUsageDTO findActiveByMemberId(int memberId) throws SQLException {
+
         return null;
     }
 
+
+    // -- 트랜잭션
     // 퇴실 처리 ended_at 기록
     public boolean updateEndedAt(int memberId)  throws SQLException {
         return false;
     }
 
+
+
     // 전체 이용 내역 조회
     public List<SeatUsageDTO> findAll() throws SQLException {
-        return null;
+        List<SeatUsageDTO> seatUsageDTOList = new ArrayList<>();
+
+        String sql = """
+                SELECT * FROM SEAT_USAGE WHERE usage_id
+                """;
+
+        try (Connection conn = DBConnectionManager.getConnection();
+            PreparedStatement pstm = conn.prepareStatement(sql)) {
+
+            try (ResultSet rs = pstm.executeQuery()) {
+                while (rs.next()) {
+                seatUsageDTOList.add(mapToUsage(rs));
+                }
+            }
+        }
+        return seatUsageDTOList;
     }
+
 
     // 회원별 이용 내역 조회
     public List<SeatUsageDTO> findByMemberId(int memberId) throws SQLException {
         return null;
+    }
+
+
+
+    public SeatUsageDTO mapToUsage(ResultSet rs) throws SQLException {
+        return SeatUsageDTO.builder()
+                .usageId(rs.getInt("usage_id "))
+                .memberId(rs.getInt("member_id"))
+                .seatId(rs.getInt("seat_id"))
+                .memberTicketId(rs.getInt("member_ticket_id"))
+                .startedAt(rs.getTimestamp("started_at").toLocalDateTime())
+                .endedAt(rs.getTimestamp("created_at").toLocalDateTime())
+                .build();
+
     }
 
 
